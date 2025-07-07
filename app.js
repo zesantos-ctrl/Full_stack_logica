5026b08a8171192e0bfb4935d571356928bc8a28
José vitor <jvitinhosa@gmail.com>
2025-07-07 11:42:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// alert("Boa vindas, ao jogo do número secreto");
+
+// let numeroSecreto = 29;
+
+// let chute = prompt('Escolha um número entre 1 e 30');
+
+// if (chute ==  numeroSecreto) {
+//     console.log('Isso ai acertou o número secretooo! (29)');
+    
+// }
+
+
+
+// alert("Erro! Preencha todos os campos");
+
+let nome = 'Lua';
+// let idade = 25;
+// let numeroDeVendas = 50;
+// let saldoDisponivel = 1000;
+
+// let mensagemDeErro = 'Erro!Preemcha todos os campos';
+// alert('mensagemDeErro');
+
+
+
+let nomeUsuario = prompt('Digite seu nome:');
+if ( nome == nomeUsuario) {
+    alert('Lua');
+}
+
+
+let idade = 18;
+
+let idadeUsuario = prompt('Digite sua idade:');
+
+if (idadeUsuario >= 18 ) {
+    alert(' pode tirar a habilitação'); 
+}  else {
+    alert( 'não pode a habilitação'); 
+}
+
+
+
+
