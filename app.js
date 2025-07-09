7766cf2935b4453c14d1dcb05f781bf4b5fbfe7e
José vitor <jvitinhosa@gmail.com>
2025-07-09 11:36:42 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,91 @@
-// alert("Boa vindas, ao jogo do número secreto");
+/*
+aula 01:
+alert("Boa vindas, ao jogo do número secreto");
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+if (chute ==  numeroSecreto) {
+    alert(`Isso ai acertou o número secretooo! ${numeroSecreto}`);
+} else {
+    alert('Você errou :(');
+}
+*/
 
-// let numeroSecreto = 29;
 
-// let chute = prompt('Escolha um número entre 1 e 30');
+/*
 
-// if (chute ==  numeroSecreto) {
-//     console.log('Isso ai acertou o número secretooo! (29)');
-    
-// }
-
-
-
-// alert("Erro! Preencha todos os campos");
+Desafio 01:
+alert("Erro! Preencha todos os campos");
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = 'Erro!Preencha todos os campos';
+alert('mensagemDeErro');
 
 let nome = 'Lua';
-// let idade = 25;
-// let numeroDeVendas = 50;
-// let saldoDisponivel = 1000;
-
-// let mensagemDeErro = 'Erro!Preemcha todos os campos';
-// alert('mensagemDeErro');
-
-
 
 let nomeUsuario = prompt('Digite seu nome:');
-if ( nome == nomeUsuario) {
+if (nome == nomeUsuario) {
     alert('Lua');
 }
-
 
 let idade = 18;
 
 let idadeUsuario = prompt('Digite sua idade:');
-
-if (idadeUsuario >= 18 ) {
-    alert(' pode tirar a habilitação'); 
-}  else {
-    alert( 'não pode a habilitação'); 
+if (idadeUsuario >= 18) {
+    alert('Pode tirar a habilitação');
+} else {
+    alert('Não pode tirar a habilitação');
 }
 
+*/
+
+/* aula02:*/ 
+
+/*
+Desafio02:
+*/
+
+//dias da semana
+// let diaDaSemana = prompt("Qual o dia da semana?");
+// if (diaDaSemana == 'sabado') {
+//     alert('Bom fim de semana!');
+// } else {
+//     alert('Boa semana!');
+// } 
+
+// if (diaDaSemana == 'domingo') {
+//     alert('Bom fim de semana!');
+// }
+ 
+//Negativo e Positivo
+// let numero = prompt('Digite seu número:');
+// if (numero > 0) {
+//     alert('positivo');
+// } else {
+//     alert('negativo');
+// }
+
+
+//pontuação:
+// let pontuação = prompt('sua pontuação:');
+
+// if (pontuação >= 100) {
+//     alert('Parabéns, você venceu');
+// } else {
+//     alert('Tente novamente para ganhar');
+// }
+
+//saldo:
+
+// let saldo = 200;
+// let seuSaldo = prompt('Digite sua senha:');
+//     alert(`seu saldo: ${saldo}`);
 
 
 
+/*
+//nome: 
+
+let nome = prompt('Digite eu nome:');
+alert('seja bem vindo!' + nome);
+*/
